20acff8925187c88b0465a69d63f371069dca60e
Maximilian Weber <weber.maximilian@googlemail.com>
2011-04-01 20:00:03 +0200

    The example has been modified in such a way that the usage of gen-class is not required anymore, since the org.cometd.server.AbstractService isn't used anymore.

--- src/hello_cometd/core.clj
+++ src/hello_cometd/core.clj
@@ -1,13 +1,38 @@
 (ns hello-cometd.core
   "A minimal hello world example how to use CometD in Clojure."
   (:use hello-cometd.jetty
-        ring.middleware.file)
+        ring.middleware.file
+        clojure.contrib.json)
   (:import
-   (org.cometd.bayeux.server BayeuxServer)
+   (org.cometd.bayeux.server BayeuxServer ServerChannel$MessageListener
+                             ConfigurableServerChannel$Initializer)
    (javax.servlet GenericServlet ServletException)
    (org.cometd.server CometdServlet AbstractService)))
 
 (def cometd-servlet (CometdServlet.))
+
+(defn process-message [message]
+  (let [json (read-json (.getJSON message))]
+    {"greeting" (str "Hello, " (get-in json [:data :name]))}))
+
+(defn add-listener [bayeux channel-id name topic]
+  (.createIfAbsent bayeux channel-id (make-array ConfigurableServerChannel$Initializer 0))
+  (let [session (.newLocalSession bayeux name)]
+    (.handshake session)
+    (let [channel (.getChannel bayeux channel-id)
+          message-listener (proxy [ServerChannel$MessageListener] []
+                             (onMessage [from channel message]
+                                        
+                                        (let [server-session (.getServerSession session)]
+                                          (if (not (= from server-session))
+                                            (do  
+                                              (.deliver from server-session topic
+                                                        (process-message message)
+                                                        nil)
+                                              true)                                                
+                                            true))
+                                           ))]
+      (.addListener channel message-listener))))
 
 (def bayeux-initializer (proxy [GenericServlet] []
                           (init [config]
@@ -16,7 +41,8 @@
                                       b (.getAttribute sc BayeuxServer/ATTRIBUTE)]
                                   (if (nil? b)
                                     (throw (Exception. "No BayeuxServer available. Servlet start order correct?"))
-                                    (comet.HelloService. b "hello"))))
+                                    (add-listener b "/service/hello" "hello" "/hello")
+                                    )))
                           (service [request response]
                                    (throw (ServletException.)))))
 
